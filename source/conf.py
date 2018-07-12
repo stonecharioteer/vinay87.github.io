5eba3faeda2cc242d56019d3d0c5fae57654570d
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-07-12 11:13:54 +0530

    Moved the html_theme_options to its own section.

--- source/conf.py
+++ source/conf.py
@@ -83,6 +83,16 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_theme_options = {
     # 'canonical_url': '',
     # 'analytics_id': '',
@@ -98,17 +108,6 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
